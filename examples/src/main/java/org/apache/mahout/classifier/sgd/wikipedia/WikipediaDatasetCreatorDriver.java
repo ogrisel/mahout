21cd2cc81dd86c4638462a4482ad82fe4f6544cc
ogrisel <olivier.grisel@ensta.org>
2010-01-19 22:51:39 +0100

    MAHOUT-228: bootstrapped work on hadoop driver to build hashed vectors representation of Wikipedia documents

---
+++ examples/src/main/java/org/apache/mahout/classifier/sgd/wikipedia/WikipediaDatasetCreatorDriver.java
@@ -0,0 +1,160 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.mahout.classifier.sgd.wikipedia;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.cli2.CommandLine;
+import org.apache.commons.cli2.Group;
+import org.apache.commons.cli2.Option;
+import org.apache.commons.cli2.builder.ArgumentBuilder;
+import org.apache.commons.cli2.builder.DefaultOptionBuilder;
+import org.apache.commons.cli2.builder.GroupBuilder;
+import org.apache.commons.cli2.commandline.Parser;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DefaultStringifier;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.SequenceFileAsBinaryOutputFormat;
+import org.apache.hadoop.util.GenericsUtil;
+import org.apache.mahout.classifier.bayes.XmlInputFormat;
+import org.apache.mahout.common.CommandLineUtil;
+import org.apache.mahout.common.FileLineIterable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Create and run the Wikipedia dataset creator suitable for stochastic gradient
+ * descent algorithms.
+ */
+public final class WikipediaDatasetCreatorDriver {
+  private static final Logger log = LoggerFactory
+      .getLogger(WikipediaDatasetCreatorDriver.class);
+
+  private WikipediaDatasetCreatorDriver() {}
+
+  public static void main(String[] args) throws IOException {
+    DefaultOptionBuilder obuilder = new DefaultOptionBuilder();
+    ArgumentBuilder abuilder = new ArgumentBuilder();
+    GroupBuilder gbuilder = new GroupBuilder();
+
+    Option dirInputPathOpt = obuilder.withLongName("input").withRequired(true)
+        .withArgument(
+            abuilder.withName("input").withMinimum(1).withMaximum(1).create())
+        .withDescription("The input directory path").withShortName("i")
+        .create();
+
+    Option dirOutputPathOpt = obuilder.withLongName("output")
+        .withRequired(true).withArgument(
+            abuilder.withName("output").withMinimum(1).withMaximum(1).create())
+        .withDescription("The output directory Path").withShortName("o")
+        .create();
+
+    Option categoriesOpt = obuilder
+        .withLongName("categories")
+        .withRequired(true)
+        .withArgument(
+            abuilder.withName("categories").withMinimum(1).withMaximum(1)
+                .create())
+        .withDescription(
+            "Location of the categories file.  One entry per line. "
+                + "Will be used to make a string match in Wikipedia Category field")
+        .withShortName("c").create();
+
+    Option helpOpt = obuilder.withLongName("help").withDescription(
+        "Print out help").withShortName("h").create();
+
+    Group group = gbuilder.withName("Options").withOption(categoriesOpt)
+        .withOption(dirInputPathOpt).withOption(dirOutputPathOpt).withOption(
+            helpOpt).create();
+
+    Parser parser = new Parser();
+    parser.setGroup(group);
+    try {
+      CommandLine cmdLine = parser.parse(args);
+      if (cmdLine.hasOption(helpOpt)) {
+        CommandLineUtil.printHelp(group);
+        return;
+      }
+
+      String inputPath = (String) cmdLine.getValue(dirInputPathOpt);
+      String outputPath = (String) cmdLine.getValue(dirOutputPathOpt);
+      String catFile = (String) cmdLine.getValue(categoriesOpt);
+      runJob(inputPath, outputPath, catFile);
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      CommandLineUtil.printHelp(group);
+    }
+  }
+
+  /**
+   * Run the job
+   *
+   * @param input
+   *          the input pathname String
+   * @param output
+   *          the output pathname String
+   * @param catFile
+   *          the file containing the Wikipedia categories
+   * @param exactMatchOnly
+   *          if true, then the Wikipedia category must match exactly instead of
+   *          simply containing the category string
+   */
+  public static void runJob(String input, String output, String catFile)
+      throws IOException {
+    JobClient client = new JobClient();
+    JobConf conf = new JobConf(WikipediaDatasetCreatorDriver.class);
+    if (log.isInfoEnabled()) {
+      log.info("Input: " + input + " Out: " + output + " Categories: "
+          + catFile);
+    }
+    conf.set("key.value.separator.in.input.line", " ");
+    conf.set("xmlinput.start", "<text xml:space=\"preserve\">");
+    conf.set("xmlinput.end", "</text>");
+    FileInputFormat.setInputPaths(conf, new Path(input));
+    Path outPath = new Path(output);
+    FileOutputFormat.setOutputPath(conf, outPath);
+    conf.setMapperClass(WikipediaRandomHasherMapper.class);
+    conf.setInputFormat(XmlInputFormat.class);
+    conf.setReducerClass(WikipediaIdentityReducer.class);
+    conf.setOutputFormat(SequenceFileAsBinaryOutputFormat.class);
+
+    FileSystem dfs = FileSystem.get(outPath.toUri(), conf);
+    if (dfs.exists(outPath)) {
+      dfs.delete(outPath, true);
+    }
+
+    Set<String> categories = new HashSet<String>();
+    for (String line : new FileLineIterable(new File(catFile))) {
+      categories.add(line.trim().toLowerCase());
+    }
+    DefaultStringifier<Set<String>> setStringifier = new DefaultStringifier<Set<String>>(
+        conf, GenericsUtil.getClass(categories));
+    String categoriesStr = setStringifier.toString(categories);
+    conf.set("wikipedia.categories", categoriesStr);
+
+    client.setConf(conf);
+    JobClient.runJob(conf);
+  }
+}
---
+++ examples/src/main/java/org/apache/mahout/classifier/sgd/wikipedia/WikipediaRandomHasherMapper.java
@@ -0,0 +1,151 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.mahout.classifier.sgd.wikipedia;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.hadoop.io.DefaultStringifier;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.GenericsUtil;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+import org.apache.lucene.wikipedia.analysis.WikipediaTokenizer;
+import org.apache.mahout.classifier.sgd.BinaryRandomizer;
+import org.apache.mahout.classifier.sgd.TermRandomizer;
+import org.apache.mahout.common.RandomUtils;
+import org.apache.mahout.math.MultiLabelVectorWritable;
+
+/**
+ * Vectorize Wikipedia Articles and use the categories tags as labels. The key
+ * of the emitted intermediate data is voluntarily randomized so to meet the
+ * I.I.D. assumption of the stochastic learner living in the reducer.
+ *
+ */
+public class WikipediaRandomHasherMapper extends MapReduceBase implements
+    Mapper<LongWritable,Text,LongWritable,MultiLabelVectorWritable> {
+
+  private static final Pattern OPEN_TEXT_TAG_PATTERN = Pattern
+      .compile("<text xml:space=\"preserve\">");
+  private static final Pattern CLOSE_TEXT_TAG_PATTERN = Pattern
+      .compile("</text>");
+
+  private List<String> inputCategories;
+  private int seed = 42;
+  private Random rng;
+  private TermRandomizer randomizer;
+  private boolean allPairs;
+  private int window;
+  private final LongWritable shuffledKey = new LongWritable();
+  private final MultiLabelVectorWritable labeledVectorValue = new MultiLabelVectorWritable();
+
+  @Override
+  public void configure(JobConf job) {
+    // seed the RNG used to shuffle the instances (wikipedia articles come in
+    // Alphabetical order and that bias could harm the convergence of online
+    // learner that assume I.I.D. samples).
+    seed = job.getInt("wikipedia.random.seed", seed);
+    rng = RandomUtils.getRandom(seed);
+    try {
+
+      // load the list of category labels to look for
+      if (inputCategories == null) {
+        List<String> newCategories = new ArrayList<String>();
+        DefaultStringifier<List<String>> setStringifier = new DefaultStringifier<List<String>>(
+            job, GenericsUtil.getClass(newCategories));
+        String categoriesStr = setStringifier.toString(newCategories);
+        categoriesStr = job.get("wikipedia.categories", categoriesStr);
+        inputCategories = setStringifier.fromString(categoriesStr);
+      }
+
+      // load the randomizer that is used to hash the term of the document
+      int probes = job.getInt("randomizer.probes", 2);
+      int numFeatures = job.getInt("randomizer.numFeatures", 80000);
+      randomizer = new BinaryRandomizer(probes, numFeatures);
+      allPairs = job.getBoolean("randomizer.allPairs", false);
+      window = job.getInt("randomizer.window", 2);
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  @Override
+  public void map(LongWritable key, Text value,
+      OutputCollector<LongWritable,MultiLabelVectorWritable> collector,
+      Reporter reporter) throws IOException {
+    shuffledKey.set(rng.nextLong());
+
+    // extract the raw markup from the XML dump slice
+    String document = StringEscapeUtils.unescapeHtml(CLOSE_TEXT_TAG_PATTERN
+        .matcher(
+            OPEN_TEXT_TAG_PATTERN.matcher(value.toString()).replaceFirst(""))
+        .replaceAll(""));
+
+    // collect the categories as indexes
+    labeledVectorValue.setLabels(findMatchingCategories(document));
+
+    // strip the wikimarkup and hash the terms using the randomizer
+    TokenStream stream = new WikipediaTokenizer(new StringReader(document));
+    List<String> allTerms = new ArrayList<String>();
+    TermAttribute termAtt = (TermAttribute) stream
+        .addAttribute(TermAttribute.class);
+    while (stream.incrementToken()) {
+      allTerms.add(termAtt.term());
+    }
+    // TODO: refactor randomizedInstance to take a token stream as input and
+    // avoid all those wasted string allocations (or prove they are harmless
+    // using the profiler).
+    labeledVectorValue.set(randomizer.randomizedInstance(allTerms, window,
+        allPairs));
+    collector.collect(shuffledKey, labeledVectorValue);
+  }
+
+  private int[] findMatchingCategories(String document) {
+    List<Integer> matchingCategories = new ArrayList<Integer>();
+    int startIndex = 0;
+    int categorystart;
+    while ((categorystart = document.indexOf("[[Category:", startIndex)) != -1) {
+      categorystart += 11;
+      int endIndex = document.indexOf("]]", categorystart);
+      if (endIndex >= document.length() || endIndex < 0) {
+        break;
+      }
+      String category = document.substring(categorystart, endIndex)
+          .toLowerCase().trim();
+      if (inputCategories.contains(category)) {
+        matchingCategories.add(inputCategories.indexOf(category));
+      }
+      startIndex = endIndex;
+    }
+    return ArrayUtils.toPrimitive(matchingCategories
+        .toArray(new Integer[matchingCategories.size()]));
+  }
+
+}
